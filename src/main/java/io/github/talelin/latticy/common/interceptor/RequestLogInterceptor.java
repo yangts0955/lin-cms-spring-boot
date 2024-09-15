8e6a8b9dce62c3ffaf0271663376d2166d2a509e
TIANSHUO YANG <neil_yang@epam.com>
2024-09-15 08:45:38 +0800

    QT CMS release 1.0

--- src/main/java/io/github/talelin/latticy/common/interceptor/RequestLogInterceptor.java
+++ src/main/java/io/github/talelin/latticy/common/interceptor/RequestLogInterceptor.java
@@ -2,7 +2,9 @@ package io.github.talelin.latticy.common.interceptor;
 
 import io.github.talelin.latticy.common.LocalUser;
 import io.github.talelin.latticy.common.util.IPUtil;
+import io.github.talelin.latticy.model.UserDO;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +18,7 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class RequestLogInterceptor implements AsyncHandlerInterceptor {
 
+    private static final String UNKNOWN_USER = "Unknown";
 
     private final ThreadLocal<Long> startTime = new ThreadLocal<>();
 
@@ -27,12 +30,17 @@ public class RequestLogInterceptor implements AsyncHandlerInterceptor {
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        String username = UNKNOWN_USER;
+        UserDO user = LocalUser.getLocalUser();
+        if (!ObjectUtils.isEmpty(user) && ObjectUtils.isEmpty(user.getUsername())) {
+            username = user.getUsername();
+        }
         log.info("[{}] -> [{}] from: {} costs: {}ms, access user: {}",
                 request.getMethod(),
                 request.getServletPath(),
                 IPUtil.getIPFromRequest(request),
                 System.currentTimeMillis() - startTime.get(),
-                LocalUser.getLocalUser().getUsername()
+                username
         );
         startTime.remove();
     }
